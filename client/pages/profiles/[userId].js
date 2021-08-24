c6d9522bd33a0eaf8a9b5c0a0bc74f197f453685
Bezula <mafciej99@gmail.com>
2021-08-24 21:02:11 +0200

    Added profile styles, getInitialProps does'nt work

--- client/pages/profiles/[userId].js
+++ client/pages/profiles/[userId].js
@@ -1,5 +1,22 @@
 const Profile = () => {
-  return <div>Profil</div>;
+  return (
+    <div className="container-lg text-muted">
+      <h1 className="display-2">Profil</h1>
+      <div className="my-5 border-bottom border-dark"></div>
+      <div className="row justify-content-center mt-5">
+        <div className="col-lg-6">
+          <p className="lead text-center border p-5">KOLUMNA 1</p>
+        </div>
+        <div className="col-lg-6 text-center">
+          <p className="lead text-center border p-5">KOLUMNA 2</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+Profile.getInitialProps = async (context, client, currentUser) => {
+  const { data } = await client.get(`./api/profiles/id/${currentUser.id}`);
 };
 
 export default Profile;
