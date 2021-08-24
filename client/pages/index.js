a9f6256ef7b252c324bf3f1ca185fa7c195f7a6f
madersky <businessmadera@gmail.com>
2021-08-24 17:16:06 +0200

    update client package.json

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,13 +1,13 @@
-import buildClient from "../api/buildClient";
+import buildClient from '../api/buildClient';
 
 const overlayHide = () => {
-  let overlay = document.getElementById("overlay");
+  let overlay = document.getElementById('overlay');
   // hide.style.transition = 'ease 1s';
   // hide.style.transform = 'translateY(200px)';
-  overlay.style.animation = "overlay-hide 1s";
+  overlay.style.animation = 'overlay-hide 1s';
 
   setTimeout(function () {
-    overlay.style.display = "none";
+    overlay.style.display = 'none';
   }, 500);
 };
 
@@ -15,29 +15,7 @@ const LandingPage = ({ currentUser }) => {
   return currentUser ? (
     <h1>You are signed in {currentUser.email}</h1>
   ) : (
-    <div className="welcome-container">
-      <div className="content">
-        <div className="loading-animation" data-text="Let'sMeet">
-          Let'sMeet
-        </div>
-        <div className="card-1">
-          Add posts! <br></br>
-          <br></br>
-          Create your own content on Let's Meet!
-        </div>
-        <div className="card-2">
-          Find friends! <br></br>
-          <br></br>Your friends is also using Let's Meet? Find them!
-        </div>
-        <div className="card-3">
-          Propose a meeting! <br></br>
-          <br></br>Create your own events, invite friends!
-        </div>
-      </div>
-      <div className="overlay" id="overlay">
-        <div className="overlay-logo" onClick={overlayHide}></div>
-      </div>
-    </div>
+    <div className="">POPRAWIĆ</div>
   );
 };
 
