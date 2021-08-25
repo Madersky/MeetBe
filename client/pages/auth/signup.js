3c6c29a447182cd2fafd3711df58307e8fe2b210
Bezula <mafciej99@gmail.com>
2021-08-25 20:39:16 +0200

    Styled signUp, installed react-bootstrap-icons in client microservice

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,73 +1,121 @@
 import { useState } from 'react';
-
 import Router from 'next/router';
+import {
+    ArrowRight,
+    PersonFill,
+    ChatRightDotsFill,
+    ShieldLockFill,
+} from 'react-bootstrap-icons';
+
 import useRequest from '../../hooks/use-request';
 
 const Signup = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [firstname, setFirstname] = useState('');
-  const [lastname, setLastname] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [firstname, setFirstname] = useState('');
+    const [lastname, setLastname] = useState('');
 
-  const { doRequest, errors } = useRequest({
-    url: '/api/users/signup',
-    method: 'post',
-    body: {
-      email,
-      firstname,
-      lastname,
-      password,
-    },
-    onSuccess: () => Router.push('/'),
-  });
+    const { doRequest, errors } = useRequest({
+        url: '/api/users/signup',
+        method: 'post',
+        body: {
+            email,
+            firstname,
+            lastname,
+            password,
+        },
+        onSuccess: () => Router.push('/'),
+    });
 
-  const onSubmit = async (event) => {
-    event.preventDefault();
-    doRequest();
-  };
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        doRequest();
+    };
 
-  return (
-    <div className="container">
-      <form onSubmit={onSubmit}>
-        <h1 className="signupin">Sign Up</h1>
-        <div className="form-group">
-          <label>E-mail Address</label>
-          <input
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            className="form-control"
-          />
+    return (
+        <div className="container-lg mt-5">
+            <div className="row justify-content-start mt-5">
+                <div className="col-4 bg-light mt-5">
+                    <form onSubmit={onSubmit}>
+                        <h1 className="lead fs-1">Sign Up</h1>
+
+                        {/* EMAIL ADDRESS */}
+
+                        <label for="email" className="form-label mt-1">
+                            E-mail Address
+                        </label>
+                        <div className="input-group">
+                            <span className="input-group-text">
+                                <ChatRightDotsFill />
+                            </span>
+                            <input
+                                type="email"
+                                id="email"
+                                placeholder="e.g. example@example.com"
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
+                                className="form-control"
+                            />
+                        </div>
+                        <label for="firstname" className="form-label mt-1">
+                            Firstname
+                        </label>
+                        <div className="input-group">
+                            <span className="input-group-text">
+                                <PersonFill />
+                            </span>
+                            <input
+                                type="text"
+                                id="firstname"
+                                placeholder="e.g. Andrew"
+                                value={firstname}
+                                onChange={(e) => setFirstname(e.target.value)}
+                                className="form-control"
+                            />
+                        </div>
+                        <label for="lastname" className="form-label mt-1">
+                            Lastname
+                        </label>
+                        <div className="input-group">
+                            <span className="input-group-text">
+                                <PersonFill />
+                            </span>
+                            <input
+                                type="text"
+                                id="lastname"
+                                placeholder="e.g Muller"
+                                value={lastname}
+                                onChange={(e) => setLastname(e.target.value)}
+                                className="form-control"
+                            />
+                        </div>
+                        <label for="password" className="form-label mt-1">
+                            Password
+                        </label>
+                        <div className="input-group">
+                            <span className="input-group-text">
+                                <ShieldLockFill />
+                            </span>
+
+                            <input
+                                id="passowrd"
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                                type="password"
+                                className="form-control"
+                            />
+                        </div>
+                        {errors}
+                        <div className="justify-content-center text-center">
+                            <button className="btn btn-light border my-5 ">
+                                <ArrowRight size={20} />
+                            </button>
+                        </div>
+                    </form>
+                </div>
+            </div>
         </div>
-        <div className="form-group">
-          <label>Firstname</label>
-          <input
-            value={firstname}
-            onChange={(e) => setFirstname(e.target.value)}
-            className="form-control"
-          />
-        </div>
-        <div className="form-group">
-          <label>Lastname</label>
-          <input
-            value={lastname}
-            onChange={(e) => setLastname(e.target.value)}
-            className="form-control"
-          />
-        </div>
-        <div className="form-group">
-          <label>Password</label>
-          <input
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            type="password"
-            className="form-control"
-          />
-        </div>
-        {errors}
-        <button className="btn-signup">Sign Up</button>
-      </form>
-    </div>
-  );
+    );
 };
 
 export default Signup;
